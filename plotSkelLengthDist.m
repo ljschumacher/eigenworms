564a3cf6f9517a5c93086bad0794ad0492d2e044
ljschumacher <lschum@me.com>
2017-02-28 17:51:00 +0000

    diagnostic plots

--- plotSkelLengthDist.m
+++ plotSkelLengthDist.m
@@ -2,14 +2,23 @@ function [ ] = plotSkelLengthDist(skelData,pixelsize,minSkelLength,filename)
 
 
 skelLengthFig = figure;
-histogram(sum(sqrt(sum((diff(skelData,1,2)*pixelsize).^2))),...
-    'Normalization','Probability','EdgeColor','none')
+skelLengths = sum(sqrt(sum((diff(skelData,1,2)*pixelsize).^2)));
+histogram(skelLengths,'Normalization','Probability','EdgeColor','none')
 hold on
 plot([minSkelLength minSkelLength],[0 0.1],'r--')
 xlabel('skeleton length (\mu m)'), ylabel('P')
 figName = strrep(filename,'_',' ');
 title(figName,'Fontweight','normal')
 
+% plot filtered skeletons on top
+axpos = get(gca,'Position');
+newpos = axpos;
+newpos(3) = 0.33*axpos(3); % third width
+overlayRejected = axes('Position',newpos,'Visible','off');
+plotSkeletons(skelData(:,:,skelLengths<minSkelLength),9,'r');
+newpos(1) = axpos(1) + 0.67*axpos(3); % start at 2/3 width
+overlayAccepted = axes('Position',newpos,'Visible','off');
+plotSkeletons(skelData(:,:,skelLengths>minSkelLength),9,'k');
 % figure export
 figFileName = ['figures/diagnostics/' strrep(strrep(figName,' ','_'),'s.hdf5','') 'lengths.eps'];
 exportfig(skelLengthFig,figFileName,'Color','rgb')
--- plotSkeletons.m
+++ plotSkeletons.m
@@ -1,33 +1,24 @@
-close all
-clear
+function [] = plotSkeletons(skelData,nFrames,color)
 
-% load some worm skeleta from tracking data and plot them
-filename = '/data1/linus/Recordings/Results/Camille_151119/CSTCTest_Ch4_19112015_121940_skeletons.hdf5';
-
-% read out the size of the dataset
-[~,h5Dims] = H5S.get_simple_extent_dims(H5D.get_space(H5D.open(...
-    H5F.open(filename),'/skeleton')));
-% NOTE: the long dimension is the first one of h5dims, but the last one of
-% the dataset
-
-% select random frame numbers (so we don't get consecutive ones)
-nPlots = 100;
-frameIDs = ceil(rand(1,nPlots)*h5Dims(1));
-
-% plot the skeleta
-figure, hold on
-% for spacing work plots
-dx = 50;
-dy = 50;
-nx = 10;
-
-for nn=1:length(frameIDs)
-    ii = frameIDs(nn);
-    % load one frame
-    skeleton = h5read(filename,'/skeleton',[1,1,ii],[2,49,1]);
-    % plot in on the next position on the 'grid'
-    x0 = mean(skeleton(1,:));
-    y0 = mean(skeleton(2,:));
-    plot(skeleton(1,:) - x0 + mod(nn,nx)*dx, ...
-        skeleton(2,:) - y0 + floor(nn/nx)*dy)
+if nargin<3
+    color = 'k';
 end
+% select random frame numbers (so we don't get consecutive ones)
+nSkeleta = size(skelData,3);
+skelData = skelData(:,:,randperm(nSkeleta,nFrames));
+
+% for spacing work plots
+dx = 200;
+dy = 200;
+nx = ceil(sqrt(nFrames));
+
+hold on
+for skelCtr=1:nFrames
+    % plot in on the next position on the 'grid'
+    x0 = mean(skelData(1,:,skelCtr));
+    y0 = mean(skelData(2,:,skelCtr));
+    plot(squeeze(skelData(1,:,skelCtr)) - x0 + mod(skelCtr-1,nx)*dx, ...
+        squeeze(skelData(2,:,skelCtr)) - y0 + floor((skelCtr-1)/nx)*dy,...
+        'Color',color)
+end
+axis equal
